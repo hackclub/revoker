a53886399757d15026bbc7912b05b65794e19682
24c02 <163450896+24c02@users.noreply.github.com>
2026-01-03 20:49:23 -0500

    add privacy page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+class StaticPagesController < ApplicationController
+  skip_forgery_protection
+
+  def privacy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "revocations#new"
   resources :revocations, only: [ :new, :create, :show, :edit, :update ]
   post "token_types/detect" => "token_types#detect"
+  get "privacy" => "static_pages#privacy"
   get "up" => "rails/health#show", as: :rails_health_check
 end
