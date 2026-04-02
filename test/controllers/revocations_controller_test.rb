2447929b6e2875af6b3ed8e931d8d5153cb6f38f
Mat Manna <matmanna@outlook.com>
2026-04-01 21:35:06 -0400

    unfix: test error

--- test/controllers/revocations_controller_test.rb
+++ test/controllers/revocations_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class RevocationsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get new_revocation_url
+    get revocations_new_url
     assert_response :success
   end
 end
