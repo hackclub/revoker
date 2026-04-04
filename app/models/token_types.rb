0495910c7806709673feb852f19c75583c80cd81
Mat Manna <matmanna@outlook.com>
2026-04-03 23:18:37 -0400

    fix: revert token_types reformatting (it was my error actually)

--- app/models/token_types.rb
+++ app/models/token_types.rb
@@ -1,18 +1,3 @@
-require_relative "token_types/base"
-require_relative "token_types/airtable_pat"
-require_relative "token_types/flavortown_api_key"
-require_relative "token_types/hack_club_ai_api_key"
-require_relative "token_types/hack_club_search_api_key"
-require_relative "token_types/hackatime_admin_key"
-require_relative "token_types/hack_club_cdn_api_key"
-require_relative "token_types/hcb_oauth"
-require_relative "token_types/slack_xoxb"
-require_relative "token_types/slack_xoxp"
-require_relative "token_types/slack_xoxc"
-require_relative "token_types/slack_xoxd"
-require_relative "token_types/theseus_api_key"
-require_relative "token_types/hackatime_token"
-
 module TokenTypes
   # Registry of all token types
   ALL = [
@@ -43,3 +28,18 @@ module TokenTypes
     token_types.map(&:display_name).join(", ")
   end
 end
+
+require_relative "token_types/base"
+require_relative "token_types/airtable_pat"
+require_relative "token_types/flavortown_api_key"
+require_relative "token_types/hack_club_ai_api_key"
+require_relative "token_types/hack_club_search_api_key"
+require_relative "token_types/hackatime_admin_key"
+require_relative "token_types/hack_club_cdn_api_key"
+require_relative "token_types/hcb_oauth"
+require_relative "token_types/slack_xoxb"
+require_relative "token_types/slack_xoxp"
+require_relative "token_types/slack_xoxc"
+require_relative "token_types/slack_xoxd"
+require_relative "token_types/theseus_api_key"
+require_relative "token_types/hackatime_token"
